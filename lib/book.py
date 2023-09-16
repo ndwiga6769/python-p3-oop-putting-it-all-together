c5d037a8d0d907e9fe0e4b60385d497bad27f313
danwycliff <danwycliff6769@gmail.com>
2023-09-17 01:06:24 +0300

    codes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count):
+        self.title = title
+        self.pages = page_count
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")  
+kigogo = Book("Kigogo",30)
     
+print(kigogo.title)
+print(kigogo.pages)
         
