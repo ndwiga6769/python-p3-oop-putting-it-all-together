47527a719ce163af396ae681a6b5891ec444b4d0
danwycliff <danwycliff6769@gmail.com>
2023-09-17 23:25:05 +0300

    codes

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    def __int__(self,brand,size):
+    def __init__(self, brand, size):
         self.brand = brand
-        self.size = size
+        self.size  = size
+
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else: 
+            print("size must be an integer")
+
+    def get_size(self):
+        return self._size
+    
+    size = property(get_size, set_size)
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
